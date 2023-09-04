9598b8b20e0d151d4ff3beb12359b1542c3132db
Tahir <mightnottaken@gmail.com>
2023-09-04 10:16:23 +0530

    logs added to mysql connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -20,19 +20,6 @@ export const AppDataSource = new DataSource({
 
 export const initializeDB = () => {
     return new Promise((res, rej) => {
-        console.log({
-            type: process.env.DB_TYPE as any,
-            host: process.env.DB_HOST,
-            port: Number(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
-            synchronize: process.env.DB_SYNCHRONIZE === "true",
-            logging: process.env.DB_LOGGING === "true",
-            entities,
-            migrations: [],
-            subscribers: [],
-        })
         AppDataSource.initialize().then(async () => {
             res(true);
         }).catch(error => {
